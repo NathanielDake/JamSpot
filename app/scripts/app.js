1d40b5caccad108226a4b6b34c7ff48887e5fb13
Nate <ndake11@gmail.com>
2017-01-15 12:43:28 +0100

    checkpoint 3 completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module('blocJams', []); //this module acts as a container for different parts of the application
+(function () {
+    
+    function config($stateProvider, $locationProvider) {
+        $locationProvider 
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+        });
+        
+        $stateProvider
+            .state('landing', {
+                url:'/',
+                templateUrl: '/templates/landing.html' 
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });
+    }
+    
+    
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+    
+    
+})();
+
+
