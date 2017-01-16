109f3e21df4e7e26e57e43b8d47d73100eceb584
Nate <ndake11@gmail.com>
2017-01-16 15:25:19 +0100

    checkpoint 5 completed

---
+++ app/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,11 @@
+(function() {
+    function LandingCtrl() {
+        this.heroTitle = "Turn the Music Up!";
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('LandingCtrl', LandingCtrl);
+    
+    
+})();
---
+++ dist/scripts/controllers/CollectionCtrl.js
@@ -0,0 +1,14 @@
+(function() {
+    function CollectionCtrl() {
+        this.albums = [];
+        for(var i = 0; i < 12; i++) {
+            this.albums.push(angular.copy(albumPicasso));
+        }
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('CollectionCtrl', CollectionCtrl);
+    
+    
+})();
